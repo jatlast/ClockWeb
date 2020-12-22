f711e6363d965450ea5a87da69c7ddc601a5a3ce
Jason T Baumbach <jatlast@hotmail.com>
2020-12-22 10:00:34 -0500

    ch15

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'crispy_forms',     # New 20201221
     'allauth',          # New 20201221
     'allauth.account',  # New 20201221
+    'debug_toolbar',    # New 20201222
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4' # New 20201221
@@ -65,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -177,3 +179,8 @@ EMAIL_USE_TLS = env("EMAIL_USE_TLS_KEY")
 
 MEDIA_URL = '/media/'                           # New 20201222
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))    # New 20201222
+
+# django-debug-toolbar
+import socket
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
