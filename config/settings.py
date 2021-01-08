ab8d020ccad6eeb539e60ceca1ae4d3d2afab6d8
Jason T Baumbach <jatlast@hotmail.com>
2021-01-07 22:09:30 -0500

    trying to get SMTP to work but getting SSL: WRONG_VERSION_NUMBER error so switching to prod to see it valid ssl certs are necessary.

--- config/settings.py
+++ config/settings.py
@@ -200,15 +200,19 @@ ACCOUNT_UNIQUE_EMAIL = True             # New 20201221
 AUTH_USER_MODEL = 'accounts.CustomUser' # New 20201220
 
 #DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'    # New 20201221
-DEFAULT_FROM_EMAIL = 'jatlast@hotmail.com'    # New 20201221
+DEFAULT_FROM_EMAIL = 'admin@miclockrepair.com'    # New 20201221
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # New 20201221
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # New 20201221
-EMAIL_HOST = env("EMAIL_HOST_KEY")
-EMAIL_HOST_USER = env("EMAIL_HOST_USER_KEY")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD_KEY")
-EMAIL_PORT = env("EMAIL_PORT_KEY")
-EMAIL_USE_TLS = env("EMAIL_USE_TLS_KEY")
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # New 20201221
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # New 20201221
+EMAIL_HOST = env("EMAIL_HOST_KEY", default="miclockrepair-com.mail.protection.outlook.com")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER_KEY", default="admin@miclockrepair.com")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD_KEY", default="SomePW")
+#EMAIL_PORT = env("EMAIL_PORT_KEY", default="586")
+#EMAIL_USE_SSL = env("EMAIL_USE_SSL_KEY", default="True")
+#EMAIL_USE_TLS = env("EMAIL_USE_TLS_KEY", default="False")
+EMAIL_PORT = 25
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 
 MEDIA_URL = '/media/'                           # New 20201222
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))    # New 20201222
