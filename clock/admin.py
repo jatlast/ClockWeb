0c6329f87e721b52d6408fe71e2e0cc616e54919
Jason T Baumbach <jatlast@hotmail.com>
2021-01-23 16:06:49 -0500

    Added fields to admin to make finding Clocktypes easier.  Added fields to the order_by for ClocktypeListView.

--- clock/admin.py
+++ clock/admin.py
@@ -5,10 +5,10 @@ from .models import Clock, Clocktypes
 #     model = Review
 
 class ClocktypesAdmin(admin.ModelAdmin):
-    list_display = ("clock_type", "footprint", "train_count",)
+    list_display = ('id','clock_type', 'footprint', 'train_count', 'chime_count', 'tube_count', 'battery_count',)
 
 class ClockAdmin(admin.ModelAdmin):
-    list_display = ("customer_fk", "clock_type_fk", "footprint", "train_count",)
+    list_display = ('customer_fk', 'clock_type_fk', 'footprint', 'train_count',)
 
 admin.site.register(Clocktypes, ClocktypesAdmin)
 admin.site.register(Clock, ClockAdmin)
